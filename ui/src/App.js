5d331522e6ad229e16a1951d821a8f7d489a5823
Nicole Trapp <futuristnicole@outlook.com>
2019-09-26 16:45:03 -0400

    add React

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+// import UserList from "./UserList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>NeoWithNic GRANDstack Starter</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
