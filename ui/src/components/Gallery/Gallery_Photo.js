88aa17502318bf11dc4135d8f154507e6954dd9e
Nicole Trapp <futuristnicole@outlook.com>
2019-09-29 14:04:41 -0400

    Gall adj

--- ui/src/components/Gallery/Gallery_Photo.js
+++ ui/src/components/Gallery/Gallery_Photo.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
+// import { Link } from 'react-router-dom';
 
 class Gallery_Photo extends Component {
     render() {
         return (
-            <div className={`gallery__item--${this.props.size}`}>
-                <img className="gallery__img" 
-                    src={`../img/Gallery/${this.props.file}.jpg`} 
-                    alt={`${this.props.alt}`} >
-                </img>
-                <p>{this.props.file}  {this.props.location} {this.props.size}</p>
-                <p>{this.props.id}</p>
-             </div>
+            // <div className={`gallery__item--${this.props.size}`}>
+                <> 
+                    <img className="gallery__img" 
+                        src={`../img/Gallery/${this.props.file}.jpg`} 
+                        alt={`${this.props.alt}`} >
+                    </img>
+                </>
+                //  <p>{this.props.file}  {this.props.location} {this.props.size}</p>
+                // <p>{this.props.id}</p> 
+            //  </div>
         )
     }
 };
